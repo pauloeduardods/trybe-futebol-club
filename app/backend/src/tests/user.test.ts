6112f42834ed2d751fa02c6c3dd2049496fdc1f1
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-03-23 19:01:26 -0300

    Tests do user

--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -6,7 +6,8 @@ import { app } from '../app';
 import User from '../database/models/User';
 
 import { Response } from 'superagent';
-import { ADMIN } from './mocks';
+import { ADMIN, ADMIN_PAYLOAD } from './mocks';
+import * as jsonwebtoken from 'jsonwebtoken';
 
 chai.use(chaiHttp);
 
@@ -17,21 +18,83 @@ describe('User Tests', () => {
 
   before(async () => {
     sinon
-      .stub(User, "findOne")
+      .stub(User, 'findOne')
       .resolves(ADMIN as User);
+    sinon
+      .stub(User, 'findByPk')
+      .resolves(ADMIN as User);
+    sinon
+      .stub(jsonwebtoken, 'sign')
+      .resolves('token');
+    sinon
+      .stub(jsonwebtoken, 'verify')
+      .resolves(ADMIN_PAYLOAD);
   });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+    (User.findByPk as sinon.SinonStub).restore();
+    (jsonwebtoken.sign as sinon.SinonStub).restore();
+    (jsonwebtoken.verify as sinon.SinonStub).restore(); 
+  });
 
-  // it('should login correctly', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app).del()
-  //   expect(...)
-  // });
+  it('should login correctly', async () => {
+    const { id, username, role, email } = ADMIN;
+    const userResponse = { id, username, role, email };
+    chaiHttpResponse = await chai
+      .request(app).post('/login').send({
+          email: ADMIN.email,
+          password: 'secret_admin',
+      });
+    expect(chaiHttpResponse.status).to.equal(200);
+    expect(chaiHttpResponse.body).to.have.property('token');
+    expect(chaiHttpResponse.body.token).to.equal('token');
+    expect(chaiHttpResponse.body).to.have.property('user');
+    expect(chaiHttpResponse.body.user).to.deep.equal(userResponse);
+    expect(chaiHttpResponse.body).to.not.have.property('password');
+  });
 
-  // it('Seu sub-teste', () => {
-  //   expect(false).to.be.eq(true);
-  // });
+  it('should not login with wrong password', async () => {
+    chaiHttpResponse = await chai
+      .request(app).post('/login').send({
+          email: ADMIN.email,
+          password: 'wrong_password',
+      });
+    expect(chaiHttpResponse.status).to.equal(401);
+  });
+  it('should not login with wrong email', async () => {
+    chaiHttpResponse = await chai
+      .request(app).post('/login').send({
+          email: 'sla',
+          password: 'secret_admin',
+      });
+    expect(chaiHttpResponse.status).to.equal(401);
+  });
+
+  it('should return the correctly role', async () => {
+    chaiHttpResponse = await chai
+      .request(app).get('/login/validate')
+      .set('Authorization', 'token');
+    console.log(chaiHttpResponse);
+    expect(chaiHttpResponse.status).to.equal(200);
+    expect(chaiHttpResponse.text).to.equal('admin');
+  });
+
+  it('should not return the correctly role', async () => {
+    (jsonwebtoken.verify as sinon.SinonStub).throws(new Error('Invalid token'));
+    chaiHttpResponse = await chai
+      .request(app).get('/login/validate')
+      .set('Authorization', 'token');
+    expect(chaiHttpResponse.status).to.equal(401);
+  });
+
+  it('should not login if DB fails', async () => {
+    (User.findOne as sinon.SinonStub).rejects(new Error('DB error'));
+    chaiHttpResponse = await chai
+      .request(app).post('/login').send({
+          email: ADMIN.email,
+          password: 'secret_admin',
+      });
+    expect(chaiHttpResponse.status).to.equal(500);
+  });
 });
